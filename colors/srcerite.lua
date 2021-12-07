8e8c41862616644b5db1209c09b79bdf2730c296
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2021-12-07 18:23:29 +0900

    Expose srcery color palette

--- colors/srcerite.lua
+++ colors/srcerite.lua
@@ -85,35 +85,7 @@ vim.g.colors_name = 'srcerite'
 	NOTE: |Replace-mode| will probably be useful here.
 ]]
 
-local srcery = {
-	black          = {'#1c1b19',  0, 'Black'},
-	red            = {'#ef2f27',  1, 'DarkRed'},
-	green          = {'#519f50',  2, 'DarkGreen'},
-	yellow         = {'#fbb829',  3, 'DarkYellow'},
-	blue           = {'#2c78bf',  4, 'DarkBlue'},
-	magenta        = {'#e02c6d',  5, 'DarkMagenta'},
-	cyan           = {'#0aaeb3',  6, 'DarkCyan'},
-	white          = {'#baa67f',  7, 'LightGray'},
-	bright_black   = {'#918175',  8, 'DarkGray'},
-	bright_red     = {'#f75341',  9, 'Red'},
-	bright_green   = {'#98bc37', 10, 'Green'},
-	bright_yellow  = {'#fed06e', 11, 'Yellow'},
-	bright_blue    = {'#68a8e4', 12, 'Blue'},
-	bright_magenta = {'#ff5c8f', 13, 'Magenta'},
-	bright_cyan    = {'#2be4d0', 14, 'Cyan'},
-	bright_white   = {'#fce8c3', 15, 'White'},
-
-	orange         = {'#ff5f00', 202, 'DarkYellow'},
-	bright_orange  = {'#ff8700', 208, 'Yellow'},
-
-	hard_black     = {'#121212', 233, 'Black'},
-	xgray1         = {'#262626', 235, 'Gray'},
-	xgray2         = {'#303030', 236, 'Gray'},
-	xgray3         = {'#3a3a3a', 237, 'DarkGray'},
-	xgray4         = {'#444444', 238, 'DarkGray'},
-	xgray5         = {'#4e4e4e', 239, 'LightGray'},
-	xgray6         = {'#585858', 240, 'LightGray'},
-}
+local srcery = require('srcerite.srcery')
 
 local black       = srcery.black
 local gray        = srcery.bright_black
