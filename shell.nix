60f08207ae340e511e60f482ab42e8e9264d8bcd
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2021-12-08 18:14:57 +0900

    feat(dev): use nix flakes
    Don't forget to do:
    
        $ echo 'use flake' > .envrc

--- shell.nix
+++ shell.nix
@@ -1,19 +1,4 @@
-let
-  fetchTarballFromGitHub = { owner, repo, rev, sha256 }:
-  builtins.fetchTarball {
-    url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
-    inherit sha256;
-  };
-
-  nixpkgs = fetchTarballFromGitHub {
-    owner = "NixOS";
-    repo = "nixpkgs";
-    rev = "78cb77b29d37a9663e05b61abb4fa09465da4b70";
-    sha256 = "0a6nv2wx82zgcw4rnvhjj0pqm0w4acbafnpj1imgjpzwss7v6w0i";
-  };
-
-  pkgs = import nixpkgs {};
-in
+{ pkgs ? import <nixpkgs> {} }:
 
 pkgs.mkShell {
   buildInputs = [
