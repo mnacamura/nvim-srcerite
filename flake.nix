f3b8600471feb78c55055937078305fe1ea90089
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2021-12-17 17:25:47 +0900

    chore: embed shell.nix to flake.nix

--- flake.nix
+++ flake.nix
@@ -4,15 +4,19 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    flake-utils.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, ... }:
   flake-utils.lib.eachDefaultSystem (system:
   let
     pkgs = nixpkgs.legacyPackages.${system};
   in
   {
-    devShell = import ./shell.nix { inherit pkgs; };
+    devShell = pkgs.mkShell {
+      buildInputs = [
+        pkgs.selene
+        # pkgs.stylua
+      ];
+    };
   });
 }
